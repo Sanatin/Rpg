e854f8c670949bfe9a58dbec7dc98f047e156e99
Sanatin <sandp02102@gmail.com>
2019-05-18 20:21:24 +0700

    Рабочий релиз

---
+++ Assets/Skripts/InteractiveZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveZone : MonoBehaviour {
+    [SerializeField] GameObject enemy;
+    [SerializeField] GameObject apperance;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Instantiate(enemy);
+        }
+        enemy.transform.position = apperance.transform.position;
+    }
+}
---
+++ Assets/Skripts/OnClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClick : MonoBehaviour {
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target);
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+
+            }
+        }
+    }
+}
